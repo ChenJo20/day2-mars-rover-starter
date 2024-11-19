67455f564fc56066f5e0d0cb2952b0e3b8ddefec
CHENJO20 <CHENJO20@oocl.com>
2024-11-19 15:47:30 +0800

    test, should_face_west_when_execute_command_given_orient_north_and_turn_left

--- MarsRover/src/test/java/MarsRoverTest.java
+++ MarsRover/src/test/java/MarsRoverTest.java
@@ -18,4 +18,14 @@ public class MarsRoverTest {
 
         assertEquals("0:0:N", report);
     }
+
+    @Test
+    public void should_face_west_when_execute_command_given_orient_north_and_turn_left() {
+        MarsRover rover = new MarsRover();
+
+        rover.executeCommand("L");
+        String report = rover.getReport();
+
+        assertEquals("0:0:W", report);
+    }
 }
