8b631e0ce80fcecb86737bee623d94fcea34a2ed
CHENJO20 <CHENJO20@oocl.com>
2024-11-19 16:19:39 +0800

    feat, implement turn left when orient north

--- MarsRover/src/main/java/MarsRover.java
+++ MarsRover/src/main/java/MarsRover.java
@@ -17,4 +17,12 @@ public class MarsRover {
     public void setDirection(String direction) {
         this.direction = direction;
     }
+
+    public void executeCommand(String command) {
+        if ("L".equals(command)) {
+            if (direction.equals("N")) {
+                direction = "W";
+            }
+        }
+    }
 }
