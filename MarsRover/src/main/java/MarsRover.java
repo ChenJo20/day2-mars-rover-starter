7bb812b099160f1eba01703f5a511545f6765205
CHENJO20 <CHENJO20@oocl.com>
2024-11-19 15:45:12 +0800

    feat, add function getReport

--- MarsRover/src/main/java/MarsRover.java
+++ MarsRover/src/main/java/MarsRover.java
@@ -1,3 +1,20 @@
 public class MarsRover {
 
+    private String direction;
+
+    MarsRover() {
+        direction = "N";
+    }
+
+    public String getReport() {
+        return "0:0:" + direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
 }
