e922aae1b133bf99ece986c197067474ad7e1b7f
CHENJO20 <CHENJO20@oocl.com>
2024-11-19 15:41:01 +0800

    test, should_return_report_when_get_report

--- MarsRover/src/test/java/MarsRoverTest.java
+++ MarsRover/src/test/java/MarsRoverTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MarsRoverTest {
@@ -7,5 +8,14 @@ public class MarsRoverTest {
     public void should_not_null_when_init_mars_rover() {
         MarsRover rover = new MarsRover();
         assertNotNull(rover);
+    }
+
+    @Test
+    public void should_return_report_when_get_report() {
+        MarsRover rover = new MarsRover();
+
+        String report = rover.getReport();
+
+        assertEquals("0:0:N", report);
     }
 }
